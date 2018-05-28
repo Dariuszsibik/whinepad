2acc291840e15a42d9051bc83fc7619c5f0d9087
Dariusz Sibik <sibikdariusz@gmail.com>
2018-05-28 19:49:57 +0200

    reactbook-boiler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Logo from './components/Logo.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <h1>
+    <Logo /> Witaj w aplikacji!
+  </h1>,
+  document.getElementById('app')
+);
