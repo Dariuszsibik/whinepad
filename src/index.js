afb2fc151992bc2c76042ea85d34770cc79ba23a
Dariusz Sibik <sibikdariusz@gmail.com>
2018-06-03 15:54:48 +0200

    update index.html in Discovery, working on Rating

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ import Suggest from './components/Suggest.js';
 ReactDOM.render(
 <div style={{padding: '20px'}}>
   <h1>Wykrywacz Komponentów</h1>
-<h2> Suggest </h2>
-<div><Suggest options ={['eenie', 'meenie', 'miney', 'mo']} /></div>
-</div>,
+<h2> Ocena </h2>
+<div> Brak wartości początkowej: <Rating /></div>
+<div> Początkowa wartość 4: <Rating defaultValue={4} /></div>
+<div> Tutaj najwyższa wartość wynosi 11: <Rating max={11} /></div>
+<div> Tylko do odczytu: <Rating readonly={true} defaultValue={3} /></div>
+</div>
+
   document.getElementById('pad')
 );
