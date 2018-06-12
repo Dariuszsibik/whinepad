8be0f8140f7ba9485bbb059af143484c7d35b3ed
Dariusz Sibik <sibikdariusz@gmail.com>
2018-06-12 20:59:37 +0200

    add Form component

--- src/index.js
+++ src/index.js
@@ -5,46 +5,20 @@ import Button from './components/Button';
 import Suggest from './components/Suggest.js';
 import Rating from './components/Rating.js';
 import FormInput from './components/Forminput.js';
+import Form from './components/Form.js';
 
 ReactDOM.render(
 <div style={{padding: '20px'}}>
   <h1>Wykrywacz Komponentów</h1>
 
+    <Form
+        fields={[
+            {label: 'Ocena', type: 'rating', id: 'rateme'},
+            {label: 'Pozdrowienia', id:'freetext'},
+        ]}
+        initialData={{rateme:4, freetext: 'Witaj'}} 
+    />
 
-<h2>Elementy Formularza</h2>
-<table><tbody>
-    <tr>
-        <td>Zwykły element formularza</td>
-        <td><FormInput/></td>
-    </tr>
-    <tr>
-        <td>Wstępnie wypełnioy</td>
-        <td><FormInput defaultValue="wartość domyślna" /></td>
-    </tr>
-    <tr>
-        <td>Wstępnie wypełniony</td>
-        <td><FormInput defaultValue="wartość domyślna" /></td>
-    </tr>
-    <tr>
-        <td>Rok</td>
-        <td><FormInput type="year" /></td>
-    </tr>
-    <tr>
-        <td>Ocena</td>
-        <td><FormInput type="rating" defaultValue={4} /></td>
-    </tr>
-    <tr>
-        <td>Sugestia</td>
-        <td><FormInput type="suggest"
-        options={['czerwony', 'zielony', 'niebieski']}
-        defaultValue="zielony" />
-        </td>
-    </tr>
-    <tr>
-        <td>Zwykły obszar tekstowy</td>
-        <td><FormInput type="text" /></td>
-    </tr>
-    </tbody></table>
 
 </div>,
 
