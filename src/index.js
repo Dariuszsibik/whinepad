84c6d6b5908ef6046ef71a88296abcfea4f4acd2
Dariusz Sibik <sibikdariusz@gmail.com>
2018-06-19 21:27:03 +0200

    Add Component Dialog

--- src/index.js
+++ src/index.js
@@ -7,14 +7,26 @@ import Rating from './components/Rating.js';
 import FormInput from './components/Forminput.js';
 import Form from './components/Form.js';
 import Actions from './components/Actions.js';
+import Dialog from './components/Dialog.js';
 
 ReactDOM.render(
 <div style={{padding: '20px'}}>
   <h1>Wykrywacz Komponentów</h1>
 
-<h2> akcje </h2>
-<div><Actions onAction={type => alert(type)} /> </div>
+<Dialog
+  header="Gotowy przykład"
+  onAction={type => alert(type)}>
+    Witaj w oknie dialogowym!
+</Dialog>
 
+<Dialog
+  header="Bez anulowania, z niestandardowym przycyskiem"
+  hasCancel={false}
+  confirmLabel="Cokolwiek"
+  onAction={type => alert(type)}>
+    Tutaj jest miejsce na cokolwiek, patrz:
+    <Button>Przycisk</Button>
+</Dialog>
 
 </div>,
 
