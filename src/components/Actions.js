cd9397437ed134a850556d930f834fab9cfba323
Dariusz Sibik <sibikdariusz@gmail.com>
2018-06-22 20:25:54 +0200

    update Actions component

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -1,30 +1,31 @@
 import PropTypes from 'prop-types'; 
 import React from 'react';
 
-const Actions = props =>
-    <div className="Actions">
-        <span tabIndex="0" 
-            className="ActionsInfo" 
-            title="Więcej info"
-            onClick={props.onAction.bind(null, 'info')}>&#8505;</span>
-        <span 
-            tabIndex="0"
-            className="ActionsEdit"
-            title="Edycja"
-            onClick={props.onAction.bind(null, 'edit')}>&#10000;</span>
-        <span 
-            tabIndex="0"
-            className="ActionsDelete"
-            title="usuń"
-            onClick={props.onAction.bind(null, 'delete')}>x</span>
-    </div>
+const Actions = props =>  
+  <div className="Actions">
+    <span 
+      tabIndex="0"
+      className="ActionsInfo"
+      title="Więcej informacji"
+      onClick={props.onAction.bind(null, 'info')}>&#8505;</span>
+    <span 
+      tabIndex="0" 
+      className="ActionsEdit" 
+      title="Edycja"
+      onClick={props.onAction.bind(null, 'edit')}>&#10000;</span>
+    <span 
+      tabIndex="0"
+      className="ActionsDelete"
+      title="Usuń"
+      onClick={props.onAction.bind(null, 'delete')}>x</span>
+  </div>
 
-    Actions.propTypes = {
-        onActions: PropTypes.func,
-    };
+Actions.propTypes = {
+  onAction: PropTypes.func,
+};
 
-    Actions.defaultProps = {
-        onActions: () => {},
-    };
+Actions.defaultProps = {
+  onAction: () => {},
+};
 
-    export default Actions
+export default Actions
