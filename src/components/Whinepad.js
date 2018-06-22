90d2bfdb50a1c5fb630cce3c6d1127ef9515bca3
Dariusz Sibik <sibikdariusz@gmail.com>
2018-06-22 20:49:49 +0200

    Update Whinepad Component

--- src/components/Whinepad.js
+++ src/components/Whinepad.js
@@ -8,121 +8,118 @@ import PropTypes from 'prop-types';
 class Whinepad extends Component {
 
     constructor(props) {
-        super(props);
-        this.state = {
-            data: props.initialData,
-            addnew: false,
-        };
-        this._preSearchData = null;
+      super(props);
+      this.state = {
+        data: props.initialData,
+        addnew: false,
+      };
+      this._preSearchData = null;
     }
-
+    
     _addNewDialog() {
-        this.setState({addnew: true});
+      this.setState({addnew: true});
     }
-
+    
     _addNew(action) {
-        if (action === 'dismiss') {
-            this.setState({addnew: false});
-            return;
-        }
-
-        let data = Array.from(this.state.data);
-        data.unshift(this.refs.form.getData());
-        this.setState({
-            addnew: false,
-            data: data,
-        });
-        this._commitToStorage(data);
+      if (action === 'dismiss') {
+        this.setState({addnew: false});
+        return;
+      }
+      let data = Array.from(this.state.data);
+      data.unshift(this.refs.form.getData());
+      this.setState({
+        addnew: false,
+        data: data,
+      });
+      this._commitToStorage(data);
     }
-
+    
     _onExcelDataChange(data) {
-        this.setState({data: data});
-        this.commitToStorage(data);
+      this.setState({data: data});
+      this._commitToStorage(data);
     }
-
+    
     _commitToStorage(data) {
-        localStorage.setItem('data', JSON.stringify(data));
+      localStorage.setItem('data', JSON.stringify(data));
     }
-
+    
     _startSearching() {
-        this._preSearchData = this.state.data;
+      this._preSearchData = this.state.data;
     }
-
+    
     _doneSearching() {
-        this.setState({
-            data: this._preSearchData,
-        });
+      this.setState({
+        data: this._preSearchData,
+      });
     }
-
+  
     _search(e) {
-        const needle = e.target.value.toLowerCase();
-        if (!needle) {
-            this.setState({data: this._preSearchData});
-            return;
+      const needle = e.target.value.toLowerCase();
+      if (!needle) {
+        this.setState({data: this._preSearchData});
+        return;
+      }
+      const fields = this.props.schema.map(item => item.id);
+      const searchdata = this._preSearchData.filter(row => {
+        for (let f = 0; f < fields.length; f++) {
+          if (row[fields[f]].toString().toLowerCase().indexOf(needle) > -1) {
+            return true;
+          }
         }
-        const field = this.props.schema.map(item => item.id);
-        const searchdata = this._preSearchData.filter(row => {
-            for (let f = 0; f < field.length; f++) {
-                if (row[field[f]].toString().toLowerCase().indexOf(needle) > -1) {
-                    return true;
-                }
-            }
-            return false;
-        });
-        this.setState({data: searchdata});
+        return false;
+      });
+      this.setState({data: searchdata});
     }
-
- render() {
-    return (
-      <div className="Whinepad">
-        <div className="WhinepadToolbar">
-          <div className="WhinepadToolbarAdd">
-            <Button 
-              onClick={this._addNewDialog.bind(this)}
-              className="WhinepadToolbarAddButton">
-              + dodaj
-            </Button>
+    
+    render() {
+      return (
+        <div className="Whinepad">
+          <div className="WhinepadToolbar">
+            <div className="WhinepadToolbarAdd">
+              <Button 
+                onClick={this._addNewDialog.bind(this)}
+                className="WhinepadToolbarAddButton">
+                + dodaj
+              </Button>
+            </div>
+            <div className="WhinepadToolbarSearch">
+              <input 
+                placeholder="Wyszukaj..." 
+                onChange={this._search.bind(this)}
+                onFocus={this._startSearching.bind(this)}
+                onBlur={this._doneSearching.bind(this)}/>
+            </div>
           </div>
-          <div className="WhinepadToolbarSearch">
-            <input 
-              placeholder="Wyszukaj..." 
-              onChange={this._search.bind(this)}
-              onFocus={this._startSearching.bind(this)}
-              onBlur={this._doneSearching.bind(this)}/>
+          <div className="WhinepadDatagrid">
+            <Excel 
+              schema={this.props.schema}
+              initialData={this.state.data}
+              onDataChange={this._onExcelDataChange.bind(this)} />
           </div>
+          {this.state.addnew
+            ? <Dialog 
+                modal={true}
+                header="Dodaj nowy element"
+                confirmLabel="Dodaj"
+                onAction={this._addNew.bind(this)}
+              >
+                <Form
+                  ref="form"
+                  fields={this.props.schema} />
+              </Dialog>
+            : null}
         </div>
-        <div className="WhinepadDatagrid">
-          <Excel 
-            schema={this.props.schema}
-            initialData={this.state.data}
-            onDataChange={this._onExcelDataChange.bind(this)} />
-        </div>
-        {this.state.addnew
-          ? <Dialog 
-              modal={true}
-              header="Dodaj nowy element"
-              confirmLabel="Dodaj"
-              onAction={this._addNew.bind(this)}
-            >
-              <Form
-                ref="form"
-                fields={this.props.schema} />
-            </Dialog>
-          : null}
-      </div>
-    );
+      );
+    }
   }
-}
-
-
-
-Whinepad.propTypes = {
+  
+  Whinepad.propTypes = {
     schema: PropTypes.arrayOf(
-        PropTypes.object
+      PropTypes.object
     ),
     initialData: PropTypes.arrayOf(
-        PropTypes.object
+      PropTypes.object
     ),
-};
-
-export default Whinepad;
+  };
+  
+  export default Whinepad
