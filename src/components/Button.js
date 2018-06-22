fc03e3bee021ac39fe1831f8e2f3d563b8130da6
Dariusz Sibik <sibikdariusz@gmail.com>
2018-06-22 20:27:46 +0200

    Update Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const Button = props => 
-    props.href
-        ?<a {...props} className={classNames('Button', props.className)} > test-must-have-content-in-component-button </a>
-        : <button {...props} className={classNames('Button', props.className)} />
+  props.href
+    ? <a {...props} className={classNames('Button', props.className)}/>
+    : <button {...props} className={classNames('Button', props.className)}/>
 
 
 Button.propTypes = {
-    href: PropTypes.string,
+  href: PropTypes.string,
 };
 
 export default Button
