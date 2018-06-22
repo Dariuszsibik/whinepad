611505dab1217023a2cb2683dd3dfb4909f2b8ec
Dariusz Sibik <sibikdariusz@gmail.com>
2018-06-22 21:16:58 +0200

    Update Rating component

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,85 +3,82 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 class Rating extends Component {
-
+    
     constructor(props) {
-        super(props);
-        this.state = {
-            rating: props.defaultValue,
-            tmpRationg: props.defaultValue,
-        };
-        this.reset = this.reset.bind(this);
-        //this.setTemp = this.reset.bind(this);
+      super(props);
+      this.state = {
+        rating: props.defaultValue,
+        tmpRating: props.defaultValue,
+      };
     }
-
+    
     getValue() {
-        return this.state.rating;
+      return this.state.rating;
     }
-
+    
     setTemp(rating) {
-        this.setState({ tmpRationg: rating});
+      this.setState({tmpRating: rating});
     }
-
+  
     setRating(rating) {
-        this.setState({
-            tmpRationg: rating,
-            rating: rating,
-        })
+      this.setState({
+        tmpRating: rating,
+        rating: rating,
+      });
     }
-
+  
     reset() {
-        this.setTemp(this.state.rating);
+      this.setTemp(this.state.rating);
     }
-
+  
     componentWillReceiveProps(nextProps) {
-        this.setRating(nextProps.defaultValue);
+      this.setRating(nextProps.defaultValue);
     }
-
+   
     render() {
-        const stars = [];
-        for (let i = 1; i <= this.props.max; i++) {
-            stars.push(
-                <span
-                className={i <= this.state.tmpRationg ? 'RatingOn' : null}
-                key={i}
-                onClick={() => !this.props.readonly && this.setRating.bind(this, i)}
-                onMouseOver={() => !this.props.readonly && this.setTemp.bind(this, i)}
-                >
-                &#9734;
-                </span>);
-        }
-        
-        return(
-            <div
-                className={ classNames({ 
-                    'Rating': true,
-                    'RatingReadonly': this.props.readonly,
-                })} 
-             onMouseOut={this.reset}
+      const stars = [];
+      for (let i = 1; i <= this.props.max; i++) {
+        stars.push(
+            <span
+            className={i <= this.state.tmpRating ? 'RatingOn' : null}
+            key={i}
+            onClick={() => !this.props.readonly && this.setRating.bind(this, i)}
+            onMouseOver={() => !this.props.readonly && this.setTemp.bind(this, i)}
             >
-                { stars }
-                { this.props.readonly || !this.props.id 
-                    ? null
-                    : <input   
-                        type="hidden"
-                        id={ this.props.id }
-                        value={this.state.rating} />
-                }
-                </div>
-        );
-    }
-}
-
-
-Rating.propTypes = {
+            &#9734;
+          </span>);
+      }
+      return (
+        <div 
+          className={classNames({
+            'Rating': true,
+            'RatingReadonly': this.props.readonly,
+          })}
+          onMouseOut={this.reset.bind(this)}
+        >
+          {stars}
+          {this.props.readonly || !this.props.id
+            ? null 
+            : <input 
+                type="hidden" 
+                id={this.props.id} 
+                value={this.state.rating} />
+          }
+        </div>
+      );
+    }  
+  }
+  
+  Rating.propTypes = {
     defaultValue: PropTypes.number,
     readonly: PropTypes.bool,
     max: PropTypes.number,
-};
-
-Rating.defaultProps = {
+  };
+  
+  Rating.defaultProps = {
     defaultValue: 0,
     max: 5,
-};
-
-export default Rating
+  };
+  
+  export default Rating
+  
