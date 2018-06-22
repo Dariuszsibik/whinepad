0955aea460e5136554ae3dc109fe9710ea01354a
Dariusz Sibik <sibikdariusz@gmail.com>
2018-06-22 20:40:59 +0200

    Update FormInput component

--- src/components/Forminput.js
+++ src/components/Forminput.js
@@ -4,47 +4,49 @@ import Suggest from './Suggest.js'
 import PropTypes from 'prop-types'; 
 
 class FormInput extends Component {
-get Value(){
-    return 'value' in this.refs.input
-        ? this.refs.input.Value
+  
+    getValue() {
+      return 'value' in this.refs.input
+        ? this.refs.input.value
         : this.refs.input.getValue();
-}
-render() {
-    const common = {
+    }
+  
+    render() {
+      const common = {
         id: this.props.id,
         ref: 'input',
         defaultValue: this.props.defaultValue,
-    };
-
-    switch (this.props.type) {
+      };
+      switch (this.props.type) {
         case 'year':
-            return(
-                <input
-                {...common}
-                type="number"
-                defaultValue={this.props.defaultValue || new Date().getFullYear()} />
-            );
+          return (
+            <input
+              {...common}
+              type="number" 
+              defaultValue={this.props.defaultValue || new Date().getFullYear()} />
+          );
         case 'suggest':
-            return <Suggest {...common} options={this.props.options} />;
+          return <Suggest {...common} options={this.props.options} />;
         case 'rating':
-            return (
-                <Rating
-                    {...common}
-                    defaultValue={parseInt(this.props.defaultValue, 10)} />
-            );
+          return (
+            <Rating
+              {...common}
+              defaultValue={parseInt(this.props.defaultValue, 10)} />
+          );
         case 'text':
-            return <textarea {...common} />;
+          return <textarea {...common} />;
         default:
-        return <input {...common} type="text" />;
+          return <input {...common} type="text" />;
+      }
     }
-}
-}
-
-FormInput.propTypes = { 
+  }
+  
+  FormInput.propTypes = {
     type: PropTypes.oneOf(['year', 'suggest', 'rating', 'text', 'input']),
     id: PropTypes.string,
     options: PropTypes.array,
-    defaultValue: PropTypes.any, 
-};
-
-export default FormInput
+    defaultValue: PropTypes.any,
+  };
+  
+  export default FormInput
+  
