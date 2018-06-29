e612c1cbd249f08a4b1bc030e6340396ae051652
Dariusz Sibik <sibikdariusz@gmail.com>
2018-06-29 19:12:05 +0200

    Update component Dialog.js

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,63 +1,62 @@
+/* @flow */
 import Button from './Button';
 import React, {Component} from 'react';
-import PropTypes from 'prop-types'; 
-import classNames from 'classnames';
 
-class Dialog extends Component {
 
-    componentWillUnmount(){
-        document.body.classList.remove('DialogModalOpen');
+type Props = {
+    header: string,
+    confirmLabel: string,
+    modal: boolean,
+    onAction: Function,
+    hasCancel: ?boolean,
+    children?: Array<any>,
+  };
+  
+  class Dialog extends Component {
+  
+    props: Props;
+    
+    static defaultProps = {
+      confirmLabel: 'ok',
+      modal: false,
+      onAction: (_) => {},
+      hasCancel: true,
+    };
+    
+    componentWillUnmount() {
+      document.body.classList.remove('DialogModalOpen');
     }
-
+    
     componentDidMount() {
-        if (this.props.modal) {
-            document.body.classList.remove('DialogModalOpen');
-        }
+      if (this.props.modal) {
+        document.body.classList.add('DialogModalOpen');
+      }
     }
-
+    
     render() {
-        return(
-            <div className={classNames({
-                'Dialog' : true,
-                'DialogModal' : this.props.modal,
-            })}>
-                <div className={this.props.modal ? 'DialogModalWrap' : null}>
-                    <div className="DialogHeader">{this.props.header}</div>
-                    <div className="DialogBody">{this.props.children}</div>
-                    <div className="DialogFooter">
-                    {this.props.hasCancel
-                    ? <span
-                        className="DialogDismiss"
-                        onClick={this.props.onAction.bind(this, 'dismiss')}>
-                        Anuluj
-                    </span>
-                    : null
-                    }
-                    <Button onClick={this.props.onAction.bind(this, this.props.hasCancel ? 'confirm' : 'dismiss')}>
-                    {this.props.confirmLabel}
-                    </Button>
-                    </div>
-                </div>
+      return (
+        <div className={this.props.modal ? 'Dialog DialogModal' : 'Dialog'}>
+          <div className={this.props.modal ? 'DialogModalWrap' : null}>
+            <div className="DialogHeader">{this.props.header}</div>
+            <div className="DialogBody">{this.props.children}</div>
+            <div className="DialogFooter">
+              {this.props.hasCancel
+                ? <span 
+                    className="DialogDismiss"
+                    onClick={this.props.onAction.bind(this, 'dismiss')}>
+                    Cancel
+                  </span>
+                : null
+              }
+              <Button onClick={this.props.onAction.bind(this, 
+                  this.props.hasCancel ? 'confirm' : 'dismiss')}>
+                {this.props.confirmLabel}
+              </Button>
             </div>
-        );
+          </div>
+        </div>
+      );
     }
-
-
-}
-
-Dialog.propTypes = {
-    header: PropTypes.string.isRequired,
-    confirmLabel: PropTypes.string,
-    modal: PropTypes.bool,
-    onAction: PropTypes.func,
-    hasCancel: PropTypes.bool
-};
-
-Dialog.defaultProps = {
-    confirmLabel: 'ok',
-    modal: false,
-    onAction: () => {},
-    hasCancel: true,
-};
-
-export default Dialog
+  }
+  
+  export default Dialog  
